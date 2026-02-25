fe7214a97ba35566e6f22b7d59d283e59db0100d
Mohammad Fakhroleslam <fakhroleslam@modares.ac.ir>
2026-02-25 07:44:30 +0330

    Add files via upload

---
+++ GRUtraining.m
@@ -0,0 +1,103 @@
+clear
+clc
+
+% Load time series data with two inputs and one target
+load prepared_data.mat
+
+% P means adsoption pressure;
+% timead means adsorption time;
+% purg means purge to feed ratio;
+
+inputs = [Timead; press; purg];
+targets = purity;
+
+% Normalize inputs and targets
+[inputs_norm, ps] = mapminmax(inputs);
+[targets_norm, ts] = mapminmax(targets);
+
+% Set the size of the training set
+% train_size = 750;
+
+trainprop=0.8;
+train_size =floor(trainprop*length(targets));
+
+% Separate data into training and test sets
+train_inputs = inputs_norm(:, 1:train_size);
+train_targets = targets_norm(:, 1:train_size);
+test_inputs = inputs_norm(:, train_size+1:end);
+test_targets = targets_norm(:, train_size+1:end);
+
+% Define the LSTM network architecture
+numFeatures = 3;
+numResponses = 1;
+numHiddenUnits = 5;
+layers = [ ...
+    sequenceInputLayer(numFeatures)
+    gruLayer(numHiddenUnits)
+    fullyConnectedLayer(1)
+    regressionLayer];
+
+% Set training options
+options = trainingOptions('adam', ...
+    'MaxEpochs',500, ...
+    'GradientThreshold',1, ...
+    'InitialLearnRate',0.05, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.5, ...    %%0.1
+    'LearnRateDropPeriod',100, ...
+    'MiniBatchSize',50, ...
+    'ValidationData',{test_inputs,test_targets}, ...
+    'ValidationFrequency',2, ...
+    'Verbose',0, ...
+    'Plots','none');
+
+% Train the LSTM network
+tic
+net = trainNetwork(train_inputs,train_targets,layers,options);
+toc
+
+% Simulate the network on the training set
+outputs_train_norm = predict(net, train_inputs);
+
+% Simulate the network on the test set
+outputs_test_norm = predict(net, test_inputs);
+
+% Denormalize the network outputs
+outputs_train = mapminmax('reverse', outputs_train_norm, ts);
+outputs_test = mapminmax('reverse', outputs_test_norm, ts);
+
+% Calculate MSE for training set
+mse_train = mse(targets(1:train_size), outputs_train);
+
+% Calculate MSE for test set
+mse_test = mse(targets(train_size+1:end), outputs_test);
+
+% Display MSE for both sets
+fprintf('MSE for training set: %f\n', mse_train);
+fprintf('MSE for test set: %f\n', mse_test);
+
+figure
+subplot(3,1,1:2)
+plot(targets, 'k',LineWidth=1.5)
+hold on
+plot((outputs_train'),'color','#B0D28A',LineWidth=1.5)
+
+plot(train_size:size(outputs_test,2)+train_size,[outputs_train(end),(outputs_test)]','m',LineWidth=1.5)
+ylim([0.88 1])
+ylabel({'Purity at the end of Adsorption Step'});
+xlabel({'Cycle Number'});
+title('\fontsize{18}Network Outputs')
+% title({'\fontsize{12}Network Outputs'; ['Training set MSE = ' num2str(mse_train)];['Test set MSE = ' num2str(mse_test)]})
+legend('Pseudo-experimental data', 'GRU output (Training Set)','GRU Output (Test Set)','Location','southwest')
+set(gca,'fontSize',18)
+
+A=abs(outputs_train-targets(1:train_size));
+B=abs(outputs_test-targets(train_size+1:end));
+C75=[A , B];
+subplot(3,1,3)
+
+bar(C75)
+ylabel({'Absolute error'});
+xlabel({'Cycle Number'});
+set(gca,'fontSize',18)
+
